c82040f2a21c3f2a9e14cd782b1989eb34057b7c
si-zero <snm306@naver.com>
2025-11-23 18:38:55 +0900

    update: URL 주소 수정

--- Minecraft/src/main/java/com/example/minecraft/controller/ServerController.java
+++ Minecraft/src/main/java/com/example/minecraft/controller/ServerController.java
@@ -5,24 +5,27 @@ import com.example.minecraft.service.ServerService;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-@WebServlet("/server")
-public class ServerController {
+@WebServlet("/server.do")
+public class ServerController extends HttpServlet {
 
     private ServerService serverService;
 
     // 초기화
     // 서블릿 객체가 최초 한 번만 실행하여 수백, 수천 개 요청이 들어와도 하나의 서비스 객체만을 재사용
+    @Override
     public void init() throws ServletException {
         this.serverService = new ServerService();
     }
 
     // 1. 서버 생성
+    @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         // JSP 에서 POST 요청이 들어왔을 때, 어떤 POST 요청인지 확인하기 위한 구문
@@ -33,6 +36,7 @@ public class ServerController {
         String version =  request.getParameter("version");
         String domain = request.getParameter("domain");
 
+        status = "ACTIVE";
         ServerDTO serverDTO = new ServerDTO(name, status, version, domain);
         boolean success = false;
 
@@ -41,7 +45,7 @@ public class ServerController {
         }
 
         if (success) {
-            response.sendRedirect(request.getContextPath() + "/server?action=" + action);
+            response.sendRedirect(request.getContextPath() + "/server.do?action=" + action);
         } else {
             System.out.println("서버 생성 실패");
         }
@@ -49,6 +53,7 @@ public class ServerController {
     }
 
     // 2. 서버 조회
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String action = request.getParameter("action");
@@ -58,7 +63,7 @@ public class ServerController {
         if (action.equals("list") || action == null) {
             ArrayList<ServerDTO> serverList = serverService.getServerListService();
             request.setAttribute("serverList", serverList);
-            viewPage = "/WEB_INF/views/server/server_list.jsp";
+            viewPage = "/WEB-INF/views/server/server_list.jsp";
         }
 
         // 2-2. 서버 단일 조회
@@ -66,11 +71,11 @@ public class ServerController {
             long id = Long.parseLong(request.getParameter("id"));
             ServerDTO server = serverService.getServerService(id);
             request.setAttribute("server", server);
-            viewPage = "/WEB_INF/views/server/server_view.jsp";
+            viewPage = "/WEB-INF/views/server/server_view.jsp";
         }
 
         else {
-            viewPage = "error.jsp";
+            viewPage = "/WEB-INF/views/error.jsp";
         }
 
         // View(JSP)로 포워딩
