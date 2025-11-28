8b8c33370aaa5134544eff73e37dd985a9eb0442
si-zero <snm306@naver.com>
2025-11-28 10:49:10 +0900

    feat: API 데이터 형식에 따른 버전 DTO 추가

---
+++ Minecraft/src/main/java/com/example/minecraft/dto/VersionDTO.java
@@ -0,0 +1,16 @@
+package com.example.minecraft.dto;
+
+public class VersionDTO {
+
+    private String name_clean;
+
+    public VersionDTO() {}
+
+    public String getName_clean() {
+        return name_clean;
+    }
+
+    public void setName_clean(String name_clean) {
+        this.name_clean = name_clean;
+    }
+}
