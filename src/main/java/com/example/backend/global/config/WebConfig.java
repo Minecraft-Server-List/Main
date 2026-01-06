add5c71d74b73f2057a783fefcecb3b1523a7c4b
si-zero <snm306@naver.com>
2026-01-06 11:00:48 +0900

    update: 이미지 불러오는 url 주소 변경

--- src/main/java/com/example/backend/global/config/WebConfig.java
+++ src/main/java/com/example/backend/global/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/Users/yusiyeong/upload/minecraft/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:/Users/yusiyeong/mcreview/uploads/");
     }
 
     @Override
