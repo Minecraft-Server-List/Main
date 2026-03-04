0a10848cb9392c1496382ec419bde1f69ec88943
si-zero <snm306@naver.com>
2026-03-04 16:27:24 +0900

    feat: 서버 상태 DTO 추가

--- src/main/java/com/example/backend/domain/server/dto/MinecraftServerStatus.java
+++ src/main/java/com/example/backend/domain/server/dto/MinecraftServerStatus.java
@@ -1,36 +1,19 @@
 package com.example.backend.domain.server.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
 public class MinecraftServerStatus {
 
-    @JsonProperty("online")
-    private boolean isOnline;
-
-    @JsonProperty("players")
-    private PlayerInfo playerInfo;
-
-    @JsonProperty("version")
-    private VersionInfo versionInfo;
+    private boolean online;
+    private Players players;
 
     @Getter
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class PlayerInfo {
-        @JsonProperty("online")
-        private int currentCount;
-
-        @JsonProperty("max")
-        private int maxCount;
-    }
-
-    @Getter
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class VersionInfo {
-        @JsonProperty("name_clean") // mcstatus.io에 맞춘 매핑이지만, 필드명은 범용적임
-        private String versionName;
+    @NoArgsConstructor
+    public static class Players {
+        private int online;
+        private int max;
     }
 }
